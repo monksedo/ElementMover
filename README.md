b19a0d75e9dc661de5d14a183a8489af0b6e1bdd
Monk Sedo <fullstackdev1828@outlook.com>
2019-10-30 14:45:29 -0700

    Update README and add code snippet png file to repo

--- README.md
+++ README.md
@@ -2,9 +2,17 @@
 A simple Element Mover - This mall app is just a simple test on the key press tracking and element moving code
 
 ## Code Snippet
+### Setup new player object
+![](01elmover.png)
+
+### Create new Element (The Read Box) on document body
+![](02elmover.png)
+
+### This block is jus to test on random color code
+![](03elmover.png)
 
 ## Load the app
-[click to run]()
+[click to run](https://monksedo.github.io/ElementMover/)
 
 ## Screenshot
 ![]()
--- app.js
+++ app.js
@@ -1,95 +1,3 @@
-/*
-var myNum1 = 1000.323498445;
-myNum1 = myNum1.toFixed(2);
-var myNum2 = "100";
-var num2 = Number(myNum2);
-console.log(myNum1);
-console.log(myNum2);
-console.log("myNum2 is now converted to number using Number() method: " + num2);
-
-var myStr = " Hello world JavaScript 123 this works, I love JavaScript ";
-
-console.log(myStr);
-console.log(myStr[9]);
-console.log(myStr[7]);
-console.log(myStr.slice(7, 12));
-console.log(myStr.substring(7, 17));
-console.log(myStr.substr(7, 12));
-
-//////////////////////////////////
-var newArr = [];
-console.log(myStr.replace("Java", "Coffee"));
-var strArr = myStr.split(" ");
-console.log(strArr);
-for (var i = 0; i < strArr.length; i++) {
-   var temp = strArr[i].replace("Java", "Coffee");
-   // strArr.replace("Java", "Coffee");
-   newArr.push(temp);
-}
-console.log(newArr);
-*/
-
-
-/*
-console.log(myStr.length);
-console.log(myStr);
-console.log(myStr.trim());
-console.log(myStr.toLowerCase().trim());
-console.log(myStr.toUpperCase());
-
-console.log(myStr.split(" ")); // Array
-console.log(myStr.split(",")); // Array
-console.log(myStr.split("a")); // Array
-console.log(myStr.charAt(9));
-*/
-
-/////////////////////////////////////////////////////
-// Get random number from min to max
-/*
-function getRan(min, max) {
-   min = Math.ceil(min);
-   max = Math.floor(max);
-   var num = Math.floor(Math.random() * (max - min + 1)) + min;
-   return exclude.includes(num) ? getRan(min, max) : num;
-}
-*/
-
-////////////////////////////////////////////////////////////////////
-// Get random number from 1 to 20 and exclude 10, 15, 7, 1, 2, 4, 5
-/*
-var include = [];
-var exclude = [15, 10, 7, 5, 4, 2, 1];
-
-for (var i = 0; i < 50; i++) {
-   var temp = getRan(1, 20);
-   if (temp === 10 || temp === 15 || temp === 7 || temp === 1 || temp === 2 || temp === 4 || temp === 5) {
-      exclude.push(temp);
-   } else {
-      include.push(temp);
-   }
-}
-
-*/
-/*
-for (var i = 0; i < 50; i++) {
-   var temp = getRan(1, 20);
-   var num = M
-      exclude.push(temp);
-   } else {
-      include.push(temp);
-   }
-}
-*/
-/*
-for (var i = 0; i < 50; i++) {
-   var min = 1; var max = 20
-   var temp = getRan(min, max);
-   include.push(temp);
-}
-console.log(exclude);
-console.log(include);
-*/
-
 /////////////////////////////////////////////////////////////////////////////////////
 // Create random color in hex value - example #ff00ff;
 
@@ -108,16 +16,10 @@ window.addEventListener("DOMContentLoaded", function (event) {
    console.log("DOM fully loaded and parsed");
 });
 
-//////////////////////////////////////////////////////////////////////////////////////
-// Track keypress and show the key vs keyCode
-/*
-window.addEventListener("keydown", function (e) {
-   var key = e.key;
-   var keyCode = e.keyCode;
-   console.log(key + " = " + keyCode);
-});
-*/
+///////////////////////////////////////////////////////////////////////////////////
+// Element Mover App
 
+// Setup a new player object
 var player = {
    speed: 100,
    x: 100,
